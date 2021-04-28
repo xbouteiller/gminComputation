390bc8f19ab3b3acc27632f85d1f3900c893a156
xavier bouteiller <bouteiller.xavier@gmail.com>
2021-04-28 14:55:31 +0200

    working program

--- gminComputation/gmincomputation.py
+++ gminComputation/gmincomputation.py
@@ -19,31 +19,38 @@ from .gmininit import ParseTreeFolder
 
 
 
-
+colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
 
 class gminComput(ParseTreeFolder):
 
     def __init__(self,
-                TIME_COL, 
-                SAMPLE_ID,
-                YVAR,
-                T,
-                RH, 
-                PATM,
-                AREA,
-                rwc_sup, 
-                rwc_inf, 
-                choices):
-        super().__init__(TIME_COL, 
-                        SAMPLE_ID,
-                        YVAR,
-                        T,
-                        RH, 
-                        PATM,
-                        AREA,
-                        rwc_sup, 
-                        rwc_inf,
-                        choices)  
+                time_col, 
+                sample_id,
+                yvar,
+                temp,
+                rh,
+                patm,
+                area,               
+                rwc_sup,
+                rwc_inf,
+                choice,
+                figfolder,
+                repfolder):
+        self.TIME_COL = time_col
+        self.SAMPLE_ID = sample_id
+        self.YVAR = yvar
+        self.T = temp
+        self.RH = rh
+        self.PATM = patm
+        self.AREA = area     
+
+        self.rwc_sup = rwc_sup
+        self.rwc_inf = rwc_inf 
+
+        self.action_choice = choice
+
+        self.fig_folder = figfolder
+        self.rep_name = repfolder
 
     def _compute_time_delta(self, df):
         # transform time to TRUE date time
@@ -58,19 +65,239 @@ class gminComput(ParseTreeFolder):
             # convert time to minute
             df['delta_time'] = df['delta_time'].dt.total_seconds() / 60 # minutes 
 
-        print(df.head())
+        # print(df.head())
 
         return df
 
     
-    def _compute_rwc():
-        pass
-    
-    def _plot_gmin():
-        pass
+    def _compute_rwc(self, df, nmean = 20,  visualise = True):           
 
-    def _compute_gmin():
-        pass
+        from matplotlib.patches import Circle, Wedge, Polygon
+
+        rwc_thressup = self.rwc_sup
+        rwc_thresinf = self.rwc_inf
+
+        dry = np.mean(df[self.YVAR].values[-int(nmean):])
+        saturated = np.mean(df[self.YVAR].values[0:nmean])## or np.max() ??
+        rwc = 100*((df[self.YVAR].values-dry)/(saturated-dry))            
+
+        def find_nearest(a, a0):
+            "Element in nd array `a` closest to the scalar value `a0`"
+            idx = np.abs(a - a0).argmin()
+            
+            return a.flat[idx]  
+
+        rwc_sup = find_nearest(rwc, rwc_thressup)
+        rwc_inf = find_nearest(rwc, rwc_thresinf)  
+
+        # print('RWC boundary: [{}% .. {}%]'.format(np.round(rwc_sup,2), np.round(rwc_inf,2)))
+        
+
+        try:
+            df['delta_time']
+        except:
+            def compute_time_lag(df):
+                df['TIME_COL2'] = pd.to_datetime(df[self.TIME_COL] , infer_datetime_format=True)  
+                # compute time delta between measures
+                # WARNING : the points need to be regurlarly sampled with a constant frequency
+                df['delta_time'] = (df['TIME_COL2']-df['TIME_COL2'][0])   
+                # convert time to minute
+                df['delta_time'] = df['delta_time'].dt.total_seconds() / 60 # minutes 
+
+                return df
+            
+            df = compute_time_lag(df)
+
+        # print(df.head(10))
+        # print(df.tail(10))
+
+
+        t80 = np.min(df.loc[rwc == rwc_sup, "delta_time"].values)
+        t50 = np.max(df.loc[rwc == rwc_inf, "delta_time"].values)
+
+        print('Detected RWC SUP {}% at {} min'.format(rwc_thressup,t80))
+        print('Detected RWC INF {}% at {} min'.format(rwc_thresinf,t50))
+        
+
+        if t80>=t50:
+            print('Warning, time at {}%: {} min, should be less than than time at {}%: {} min'.format(rwc_thressup,t80, rwc_thresinf,t50))
+            print('keeping full data set')
+
+            t80 = df['delta_time'].values[0]
+            t50 = df['delta_time'].values[-1]
+
+            rwc_sup = 100
+            rwc_inf = 0
+
+            print('New: Detected RWC SUP 0% at {} min'.format(t80))
+            print('New: Detected RWC INF 100% at {} min'.format(t50))
+        
+
+        TITLE = str(df[self.SAMPLE_ID].unique()[0])            
+            
+        fig, ax1 = plt.subplots()
+
+        plt.title(TITLE)
+        color = 'tab:blue'
+        ax1.set_xlabel('time (min)')
+        ax1.set_ylabel(TITLE + '\nWeight (g)', color=color)
+        ax1.plot(df['delta_time'], df[self.YVAR], color=color, linestyle='-', marker='.', label = 'data', alpha = 0.5)
+        ax1.tick_params(axis='y', labelcolor=color)
+        ax1.set_ylim([0.9*np.min(df[self.YVAR]), 1.1*np.max(df[self.YVAR])])        
+
+        verts = [[0, 0],[t80, 0], [t80, 1.1*np.max(df[self.YVAR].values)] , [0, 1.1*np.max(df[self.YVAR].values)]]
+        poly = Polygon(verts, facecolor='r', alpha = 0.5)
+        ax1.add_patch(poly)   
+
+        verts = [[t50, 0],[np.max(df['delta_time'].values), 0], [np.max(df['delta_time'].values), 1.1*np.max(df[self.YVAR].values)], [t50, 1.1*np.max(df[self.YVAR].values)] ]
+        poly = Polygon(verts, facecolor='r', alpha = 0.5)
+        ax1.add_patch(poly)
+
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        color = 'tab:green' 
+        ax2.set_ylabel('RWC (%)', color=color)  # we already handled the x-label with ax1
+        ax2.plot(df['delta_time'],  rwc, color=color, marker='.', label = 'RWC', alpha = 0.5)
+        ax2.tick_params(axis='y', labelcolor=color) 
+
+        # Make the shaded region
+        figname = self.fig_folder + '/' + 'rwc' + '/' + TITLE + '.png'
+        plt.savefig(figname, dpi = 420, bbox_inches = 'tight')
+        # plt.pause(PAUSE_GRAPH)
+        #plt.show()
+        if visualise:
+            # plt.show()
+            plt.waitforbuttonpress(0)
+            # input()
+        plt.close() 
+
+        print('Slicing df between RWC80 and RWC50')
+        # df = df[(rwc < rwc_thressup) & (rwc > rwc_thresinf)].copy()
+        df = df[ (df.delta_time.values <= t50) & (df.delta_time.values >= t80)].copy()
+
+        print('t min : {} min'.format(df.delta_time.min().round(3)))
+        print('t max : {} min'.format(df.delta_time.max().round(3)))
+
+        return df, t80, t50, rwc_sup, rwc_inf
+    
+
+    def _plot_gmin(self, df):
+        '''
+        detection of the crossing point of the rmse error from the exp & linear fitting
+        yexp & ylin : rmse from the sliding window
+        '''
+        gmin_mean=''
+        list_of_param=['', '', '', '', '', '']      
+
+       
+        # plot the figure of the detected crossing sections  
+        #  
+        TITLE = str(df[self.SAMPLE_ID].unique()[0])            
+            
+        fig, ax1 = plt.subplots()
+
+        plt.title(TITLE)
+        color = 'tab:blue'
+        ax1.set_xlabel('time (min)')
+        ax1.set_ylabel(TITLE + '\nWeight (g)', color=color)
+        ax1.plot(df['delta_time'], df[self.YVAR], color=color, linestyle='-', marker='.', label = 'data', alpha = 0.5)
+        ax1.tick_params(axis='y', labelcolor=color)
+        ax1.set_ylim([0.9*np.min(df[self.YVAR]), 1.1*np.max(df[self.YVAR])])      
+        
+           
+        if (self.action_choice =='1'):
+            print('\nPlease select two points on the figure')
+            # plt.waitforbuttonpress(0)  
+            selected_points = fig.ginput(2)
+            slope, intercept, rsquared, fitted_values, Xreg = self._compute_slope(df,selected_points[0], True, selected_points[1])
+            ax1.plot(Xreg, fitted_values, c = colors['black'], lw = 2) 
+            gmin_mean, list_of_param = self._compute_gmin(df=df, slope=slope, t1=selected_points[0][0], t2 = selected_points[1][0]) 
+
+        else:
+            Xidx = df['delta_time'].values[0]
+            slope, intercept, rsquared, fitted_values, Xreg = self._compute_slope(df,Xidx1=Xidx)
+            ax1.plot(Xreg, fitted_values, c = colors['black'], lw = 2)
+            gmin_mean, list_of_param = self._compute_gmin(df=df, slope=slope, t1=Xidx, t2 = None)        
+            
+            # close the graph on a click
+            # plt.pause(PAUSE_GRAPH)
+        if (self.action_choice =='2') | (self.action_choice =='1') :
+            plt.waitforbuttonpress(0)
+
+        figname = self.fig_folder + '/' + 'gmin' + '/' + TITLE + '.png'
+        plt.savefig(figname, dpi = 420, bbox_inches = 'tight')
+
+        # input()
+        plt.close()   
+
+        
+        if (self.action_choice =='1') :
+            gs = [TITLE, [i[0] for i in selected_points ], slope, rsquared, gmin_mean, list_of_param]
+        else:       
+            gs = [TITLE, Xidx, slope, rsquared, gmin_mean, list_of_param]
+        return gs
+
+
+
+    def _compute_gmin(self, df, slope, t1, t2 = None):
+        '''
+        compute gmin on a slice of a signal
+        use the slope value
+        '''
+        if t2 is None:
+            df = df[df['delta_time']>= t1].copy()
+        if t2 is not None:            
+            df = df[(df['delta_time']>= t1) & (df['delta_time']<= t2)].copy()
+
+        k= (slope/18.01528)*(1000/60) #ici c'est en minutes (60*60*24)
+
+        #Calcul VPD en kpa (Patm = 101.325 kPa)
+        VPD =0.1*((6.13753*np.exp((17.966*np.mean(df[self.T].values)/(np.mean(df[self.T].values)+247.15)))) - (np.mean(df[self.RH].values)/100*(6.13753*np.exp((17.966*np.mean(df[self.T].values)/(np.mean(df[self.T].values)+247.15)))))) 
+
+        #calcul gmin mmol.s
+        gmin_ = -k * np.mean(df[self.PATM].values)/VPD
+
+        #calcul gmin en mmol.m-2.s-1
+        gmin = gmin_ / np.mean(df[self.AREA].values)
+
+        print('gmin_mean: ', np.round(gmin, 3))
+
+        return gmin, [k, VPD, np.mean(df[self.T].values), np.mean(df[self.RH].values), np.mean(df[self.PATM].values), np.mean(df[self.AREA].values)]
+
+    def _compute_slope(self, df, Xidx1, interval = False, *args, **kwargs):
+        '''
+        fit a linear regression on a slice of a signal and return slope, intercept, rsqquared and fitted values
+        '''         
+        # this is useful for slicing X value returned by ginput
+        # if len(Xidx1)>1:
+        #     Xidx1 = Xidx1[0]  
+
+        # print('Xidx1: ', Xidx1)     
+
+        Xselected = df['delta_time'].values
+        yselected = df[self.YVAR].values
+
+        if interval: 
+            Xidx1 = Xidx1[0]                      
+            Xidx2 = args[0]
+            if len(Xidx2)>1:
+                Xidx2 = Xidx2[0]
+
+            print('t min : {} min'.format(np.round(Xidx1,3)))
+            print('t max : {} min'.format(np.round(Xidx2,3)))
+            # print('Xidx1: ', Xidx1)
+            # print('Xidx2: ', Xidx2)   
+            X = Xselected[(Xselected >= Xidx1) & (Xselected <= Xidx2)].copy()
+            y = yselected[(Xselected >= Xidx1) & (Xselected <= Xidx2)].copy()
+        else:
+            X = Xselected[Xselected >= Xidx1].copy()
+            y = yselected[Xselected >= Xidx1].copy()
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(X, y)
+
+        fitted_values = slope*X + intercept
+        rsquared = r_value**2
+
+        return slope, intercept, rsquared, fitted_values, X
 
     
 
--- gminComputation/gmininit.py
+++ gminComputation/gmininit.py
@@ -219,19 +219,25 @@ class ParseTreeFolder():
     
     def run(self):
         '''Display the menu and respond to choices.'''
-        
+        self.firstchoice = 0
         while True:
             self.display_menu()
-            choice = input("Enter an option: ")
+            if self.firstchoice == 0:
+                choice = input("Enter an option: ")
+            else:
+                choice = '4'
 
             # redirection to the self.choices attribute in the __init__
             action = self.choices.get(choice)
+
+            self.action_choice = choice
 
             if action:
                 action()
             else:
                 print("{0} is not a valid choice".format(choice))
                 self.run()
+            self.firstchoice += 1
 
 
     def _quit(self):
@@ -325,8 +331,8 @@ class ParseTreeFolder():
                 fig_folder = starting_name+'_'+str(i)
                 if not os.path.exists(fig_folder):
                     os.makedirs(fig_folder)
-                    os.makedirs(fig_folder+'/'+'rmse')
-                    os.makedirs(fig_folder+'/'+'diff')
+                    os.makedirs(fig_folder+'/'+'gmin')
+                    # os.makedirs(fig_folder+'/'+'diff')
                     os.makedirs(fig_folder+'/'+'rwc')
                     break
             self.fig_folder = fig_folder 
@@ -348,218 +354,121 @@ class ParseTreeFolder():
             self.rep_name = temp_name
 
 
-
-
-
-    # def _parse_samples(self, dffile): 
-    #     '''
-    #     for each file, each unique ID will be analyzed
-    #     using a FUNC
-        
-    #     this function will ask if we want to work on raw or smoothed data
-    #     '''
-
-
-
-    #     # for each file, slice each unique ID
-    #     for sample in dffile[self.SAMPLE_ID].unique():            
-    #         self.sample = sample
-    #         df = dffile.loc[dffile[self.SAMPLE_ID]==sample,:].copy().reset_index()
-    #         return df
-    
-            # # transform time to TRUE date time
-            # try:
-            #     df['delta_time']
-            # except:
-            #     print('delta time column is leaking ... computing ...')
-            #     df['TIME_COL2'] = pd.to_datetime(df[self.TIME_COL] , infer_datetime_format=True)  
-            #     # compute time delta between measures
-            #     # WARNING : the points need to be regurlarly sampled with a constant frequency
-            #     df['delta_time'] = (df['TIME_COL2']-df['TIME_COL2'][0])   
-            #     # convert time to minute
-            #     df['delta_time'] = df['delta_time'].dt.total_seconds() / 60 # minutes 
-
-            # ################# PUT HERE THE RWC METHOD
-            # df, t80, t50 = self._compute_rwc(df)
-            # self.t80 = t80
-            # self.t50 = t50
-            # #########################################
-
-            # self.Xselected = df['delta_time'].values
-            # self.yselected = df[self.YVAR].copy().values
-            # #print(FRAC_P)
-            # #print(DELTA_MULTI)
-            # self.Ysmooth = self._smoother(self.Xselected , self.yselected, fr = FRAC_P, delta_multi = DELTA_MULTI)
-
-            # df['raw_data'] = df[self.YVAR].copy()
-            # df['smooth_data'] = self.Ysmooth.copy()
-            # df['Work_on_smooth'] = 'No'
-
-            # if not self.Conductance or self.Conductance:  
-            #     # strange code : but always ask for smoothing
-            #     # future : can be modified (either always allow smoothing and self.conductance is not useful or smoothinf is forbiden for robust differential)
-            #     plt.plot(self.Xselected, self.yselected, linestyle='-', marker='.', label = 'raw data')
-            #     plt.plot(self.Xselected, self.Ysmooth, linestyle='-', marker='.', label = 'smooth')
-            #     plt.title(self.sample)
-            #     plt.ylabel(self.sample)
-            #     plt.legend()    
-            #     # plt.pause(PAUSE_GRAPH/10)
-            #     plt.waitforbuttonpress(0)
-            #     # input()
-            #     plt.close() 
-
-            
-            #     print('''
-            #     Do you want to work on smoothed data ?
-
-            #     1: Yes
-            #     2: Yes, But I want to adjust smoothing parameters
-            #     3: No            
-            #     ''') 
-
-            #     what_to_do = self._get_valid_input('What do you want to do ? Choose one of : ', ('1','2','3'))
-            #     ########################################################################
-            #     if what_to_do=='1':
-            #         df[self.YVAR] = self.Ysmooth.copy()
-            #         df['Work_on_smooth'] = 'yes'
-            #     if what_to_do=='2':
-            #         while True:          
-            #             while True:
-                            
-            #                 try:
-            #                     _FRAC=0.1
-            #                     FRAC_P2 = float(input('What is the frac value ? (current value : {}) '.format(_FRAC)) or _FRAC)
-            #                     _FRAC = FRAC_P2
-            #                     break
-            #                 except ValueError:
-            #                     print("Oops!  That was no valid number.  Try again...")                    
-            #             while True:
-
-            #                 try:
-            #                     _DELTA_MULTI=0.01
-            #                     DELTA_MULTI2= float(input('What is the delta value ? (current value : {}) '.format(_DELTA_MULTI)) or _DELTA_MULTI)
-            #                     _DELTA_MULTI = DELTA_MULTI2
-            #                     break
-            #                 except ValueError:
-            #                     print("Oops!  That was no valid number.  Try again...")
-
-            #             self.Ysmooth = self._smoother(self.Xselected , self.yselected, fr = FRAC_P2, delta_multi = DELTA_MULTI2)
-            #             plt.plot(self.Xselected, self.yselected, linestyle='-', marker='.', label = 'raw data')
-            #             plt.plot(self.Xselected, self.Ysmooth, linestyle='-', marker='.', label = 'smooth')
-            #             plt.legend()    
-            #             # plt.pause(PAUSE_GRAPH/10)
-            #             plt.waitforbuttonpress(0)
-            #             # input()
-            #             plt.close()   
-            #             print('''
-            #             Do you want to keep this values?
-
-            #             1: Yes
-            #             2: No            
-            #             ''')
-            #             what_to_do = self._get_valid_input('What do you want to do ? Choose one of : ', ('1','2'))
-            #             if what_to_do == '1':
-            #                 break
-            #             if what_to_do == '2':
-            #                 pass
-                    
-            #         df[self.YVAR] = self.Ysmooth.copy()
-            #         df['Work_on_smooth'] = 'yes'
-        
-            # FUNC(df)
-
-
     def _execute_computation(self):
         '''
         parse all the files within the folder tree       
         '''
 
-        print('\n Starting Gmin computation\n')
+        print('\nStarting Gmin computation\n')
 
         
         from .gmincomputation import gminComput
 
-        print('\nchoices is {}\n'.format(self.choices))
+        print('\nchoices is {}\n'.format(self.action_choice ))
 
         self._create_saving_folder()
 
         dimfolder = len(self.listOfFiles)
-        li_all = []
+        # li_all = []
+        list_of_df = []
+        global_score = []
+
+        # print(self.listOfFiles)
+        # time.sleep(2)
 
         for d in np.arange(0,dimfolder):
             print('\n\n\n---------------------------------------------------------------------')
             print(d)            
-            li = []
+            
             try:
                 self.presentfile=self.listOfFiles[d][0]
             except:
-                self.presentfile = 'No file'
+                self.presentfile = 'No file'            
             
-            self.df_rmse = None
-            self.df_value = None
             print('parsing list of files from : {}'.format(self.presentfile))
-            
 
-            list_of_df = []
             if self.presentfile != 'No file':
                 for elem in self.listOfFiles[d]:
-                    dffile = self._robust_import(elem) 
-
-                    print(dffile.head())
-                    print(dffile.shape)
+                    dffile = self._robust_import(elem)
+                    # remove the .csv extension from the name                    
+                    temp_folder = os.path.splitext(str(os.path.basename(elem)))[0]  
+                    # print('tf1: ', temp_folder)                 
 
                     for sample in dffile[self.SAMPLE_ID].unique():            
                         self.sample = sample
                         df = dffile.loc[dffile[self.SAMPLE_ID]==sample,:].copy().reset_index()
+                        # Analysing
+                        print('Analysing: {}'.format(sample))
 
-                        gmc = gminComput(self)
+                        # initialising gmin computation class
+                        gmc = gminComput(self.TIME_COL,
+                                        self.SAMPLE_ID,
+                                        self.YVAR,
+                                        self.T,
+                                        self.RH,
+                                        self.PATM,
+                                        self.AREA,
+                                        self.rwc_sup,
+                                        self.rwc_inf, 
+                                        self.action_choice,
+                                        self.fig_folder,
+                                        self.rep_name)
+                        # computing time delta
+                        df = gmc._compute_time_delta(df)
 
-                        gmc._compute_time_delta(df)
+                        # computing RWC
+                        if (self.action_choice == '2') | (self.action_choice == '3'):
+                            print('Computing RWC')
+                            if self.action_choice == '2':
+                                df, t80, t50 = gmc._compute_rwc(df)
+                            else:
+                                df, t80, t50, rwc_sup, rwc_inf = gmc._compute_rwc(df, visualise = False)
+                            # self.t80 = t80
+                            # self.t50 = t50
+                        else:
+                            t80 = None
+                            t50 = None
+                            rwc_sup = None
+                            rwc_inf = None
 
-                
-                    
-            #         # fit after rwc removal, if you want to use the full curve set tinf to 0 and tsup to 100
-            #         # dffile, t80, t50 = self._compute_rwc(dffile)
-                    
-            #         # future : do i need to use global var as self.globalscore ... ?
-            #         self._parse_samples(dffile = dffile, FUNC = self._change_det)
-            #         temp_df = pd.DataFrame(self.global_score, columns = ['Sample_ID', 'Change_points','slope', 'Rsquared', 'Gmin_mean', 'pack'])
-            #         temp_df2 = pd.DataFrame(temp_df["pack"].to_list(), columns=['K', 'VPD', 'mean_T', 'mean_RH', 'mean_Patm', 'mean_area'])
-            #         temp_df = temp_df.drop(columns='pack')
-                    
-            #         # remove the .csv extension from the name
-            #         temp_folder = os.path.splitext(str(os.path.basename(elem)))[0]
+                        # plotting gmin
+                        gs = gmc._plot_gmin(df) 
 
-            #         if not os.path.exists(temp_name + temp_folder ):
-            #             os.makedirs(temp_name + temp_folder)
-            #         # concat df
-            #         temp_df = pd.concat([temp_df,temp_df2], axis = 1)
-            #         temp_df['Campaign'] = temp_folder
+                        global_score.append(gs)   
 
-            #         # NEW ##
-            #         temp_df['fit_mode'] = self.fit_exp_rmse
-            #         temp_df['transfo_mode'] = self.transfo_rmse
+                        temp_df = pd.DataFrame(global_score, columns = ['Sample_ID', 'Interval_time','slope', 'Rsquared', 'Gmin_mean', 'pack'])
+                        temp_df2 = pd.DataFrame(temp_df["pack"].to_list(), columns=['K', 'VPD', 'mean_T', 'mean_RH', 'mean_Patm', 'mean_area'])
+                        temp_df = temp_df.drop(columns='pack')
+                        temp_df = pd.concat([temp_df,temp_df2], axis = 1)
 
-            #         temp_df['percentage_rwc_sup']=self.rwc_sup
-            #         temp_df['percentage_rwc_inf']=self.rwc_inf
+                        # print('tf2: ', temp_folder) 
+                        temp_df['Campaign'] = temp_folder
+                        
+                        temp_df['percentage_rwc_sup']=rwc_sup
+                        temp_df['percentage_rwc_inf']=rwc_inf
 
-            #         temp_df['time_rwc_sup']=self.t80
-            #         temp_df['time_rwc_inf']=self.t50
+                        temp_df['time_rwc_sup']=t80
+                        temp_df['time_rwc_inf']=t50
 
-            #         # append df to list
-            #         list_of_df.append(temp_df)
-            #         temp_df.to_csv(temp_name + temp_folder + '/RMSE_detection_' + str(os.path.basename(elem))) 
-            #         #pd.concat([temp_df,temp_df2], axis = 1).to_csv('output_files/'+ temp_folder + '/RMSE_detection_' + str(os.path.basename(elem)))                 
-                                        
-            #         self.df_rmse.to_csv(temp_name+ temp_folder + '/RMSE_score_'+str(os.path.basename(elem)))
-            #         self.df_value.to_csv(temp_name+ temp_folder + '/RMSE_df_complete_'+str(os.path.basename(elem)))
-            #         self.df_rmse = None
-            #         self.df_value = None
-            #         self.global_score = []
+                        if (self.action_choice == '2') | (self.action_choice == '3'):
+                            temp_df['Mode']='RWC filtered'
+                        else:
+                            temp_df['Mode']='Manual Selection'
 
-            # # save the appended df in a global file
-            # # explode remove the square bracket [] from cells and convert to multiline
-            # pd.concat(list_of_df).reset_index().explode('Change_points').to_csv(temp_name+'RMSE_df_complete_full.csv')
-            # pd.concat(list_of_df).reset_index().explode('Change_points').drop_duplicates(subset=['Campaign','index','Sample_ID','slope']).to_csv(temp_name+'RMSE_df_complete_full_No_duplicates.csv')
-                
-    
+                        print('\n')
+
+                        # append df to list
+                    list_of_df.append(temp_df)
+                    global_score = []
+            else:
+                pass
+
+        # save the appended df in a global file
+        # explode remove the square bracket [] from cells and convert to multiline
+        # pd.concat(list_of_df).reset_index().explode('Interval_time').to_csv(self.rep_name+'/RMSE_df_complete_full.csv')
+        pd.concat(list_of_df).reset_index().explode('Interval_time').to_csv(self.rep_name+'/RMSE_df_complete_full.csv')
+        pd.concat(list_of_df).reset_index().explode('Interval_time').drop_duplicates(subset=['Campaign','index','Sample_ID','slope']).to_csv(self.rep_name+'/RMSE_df_complete_full_No_duplicates.csv')
+            
+        
+
+                        
